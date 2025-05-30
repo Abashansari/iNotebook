dedef0d478963a627b1eee48c866575127f51d50
Abash Ansari <ansariabash2004@gmail.com>
2025-05-30 12:52:18 -0400

    created registerValidation for user's Registration form

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -7,37 +7,42 @@ const router = express.Router()
 
 const registerValidation = [
     body('userName').notEmpty().withMessage('Username is required'),
-    body('email').isEmail().withMessage('Invalid email'),
+
+    body('email').isEmail().withMessage('Invalid email')
+
+        .custom(async (value) => {                                               // checking if user is already exist ??
+            const userExist = await User.findOne({ email: value })
+            if (userExist) {
+                throw new Error("Email is in use")         // Throw error instead of res.send()
+            }
+        }),
     body('password').isStrongPassword().withMessage('Weak password')
 ]
 
 
-router.post('/',registerValidation, async (req,res)=>{
+router.post('/', registerValidation, async (req, res) => {
     const error = validationResult(req)
-    if(!error.isEmpty()){
-        res.send(422).json({errors:error.array})
+
+    if (!error.isEmpty()) {
+        res.status(422).json({ errors: error.array() })
     }
-    res.send("user created")
-
-
-
-
+    // res.send("user created")
 
     try {
-        const {userName,email,password} = req.body
+        const { userName, email, password } = req.body
 
         const user = new User({
             userName,
             email,
             password
-          })
-          await user.save() 
-          // store data in mongodb
-          res.send(req.body)
-          res.status(201).send(user)
+        })
+        await user.save()   // store data in mongodb
+
+        res.send(req.body)                //for debugging (to see what server has recived)
+        res.status(201).send(user)        //returning a newly created resource.
 
     } catch (error) {
-        res.status(400).send({error:error.message})
+        res.status(400).send({ error: error.message })
     }
 })
 export default router
