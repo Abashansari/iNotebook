c72ce09e0f2443981a7e8d779ec037756b51db31
Abash Ansari <ansariabash2004@gmail.com>
2025-06-01 05:54:53 -0400

    added a user element in NoteSchema object to  link user and notes

--- src/backend/modules/Notes.js
+++ src/backend/modules/Notes.js
@@ -1,12 +1,30 @@
 import mongoose from 'mongoose'
+import { Schema } from 'mongoose';
 // import userSchema from './Schema'
 
 const NoteSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-    title: String,
-    description: String,
-    tag: String,
-    date: { type: Date, default: Date.now }
+    user: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'user'
+       },
+    title:{
+      type:String,
+      required: true
+    },
+    description: {
+      type:String,
+      required: true
+    },
+    tag: {
+      type:String,
+      default: "General"
+    },
+    date: {
+       type: Date,
+        default: Date.now 
+      }
   });
 
-export default NoteSchema
+const notes = mongoose.model('notes',NoteSchema)
+
+export default notes
