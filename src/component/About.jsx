c4c0ba165258e3725f0b10c08d377f9e303ae57e
Abash Ansari <ansariabash2004@gmail.com>
2025-06-02 09:01:45 -0400

    imported NoteState

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {NoteState} from '../context/notes/NoteState'
 
 export default function About() {
+  const a = NoteState()
   return (
-    <div> This is About</div>
+    <div> This is, {a.user}</div>
   )
 }
