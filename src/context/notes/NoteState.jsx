da8852ea62c1c9a07567ae2083a2477c16168dd8
Abash Ansari <ansariabash2004@gmail.com>
2025-06-03 03:59:44 -0400

    document format

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useState } from "react";
 import NoteContext from "./NoteContext";
 
-export default function NoteState({children}){
-    const [user,setUser] = useState("Abash Ansari")
-    return(
-        <NoteContext.Provider value={{user,setUser}}>
-        {children}
+export default function NoteState({ children }) {
+    const [user, setUser] = useState("Abash Ansari")
+
+    return (
+        <NoteContext.Provider value={{ user, setUser }}>
+            {children}
         </NoteContext.Provider>
     )
 }
