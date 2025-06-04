cf97ac458b9b981fa08a895098152790c944af15
Abash Ansari <ansariabash2004@gmail.com>
2025-06-04 03:50:20 -0400

    initialed a note for debugging/test the program

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,13 +1,39 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 export default function NoteState({ children }) {
-    const [user, setUser] = useState("Abash Ansari")
+  const initialNotes = [
+    {
+      _id: "683fe7fc1484545b5b21ba56",
+      user: "683ae0203f23d6a97c2b7dff",
+      title: "Love in the Time of Cholera",
+      description: "A note about the book.",
+      tag: "Book",
+      date: "2025-06-04T06:30:20.854Z",
+    },
+    {
+        _id: "683fe7fc1484545b5b21ba57",
+        user: "683ae0203f23d6a97c2b7dfg",
+        title: " Time of Cholera",
+        description: "A note about the book.",
+        tag: "Book",
+        date: "2025-06-04T06:30:20.854Z",
+      },
+      {
+        _id: "683fe7fc1484545b5b21ba58",
+        user: "683ae0203f23d6a97c2b7dfh",
+        title: "Love in Test",
+        description: "A note about the book.",
+        tag: "Book",
+        date: "2025-06-04T06:30:20.854Z",
+      },
+  ];
 
-    return (
-        <NoteContext.Provider value={{ user, setUser }}>
-            {children}
-        </NoteContext.Provider>
-    )
+  const [notes, setNotes] = useState(initialNotes);
+
+  return (
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {children}
+    </NoteContext.Provider>
+  );
 }
