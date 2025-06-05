3925ae27c932f9b29242254ac8c0ce6410e33a5f
Abash Ansari <ansariabash2004@gmail.com>
2025-06-05 04:26:55 -0400

    include a addNote function

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -31,8 +31,37 @@ export default function NoteState({ children }) {
 
   const [notes, setNotes] = useState(initialNotes);
 
+
+  // ---------ADD a note------------------------>
+
+  const addNote = (title,description,tag) =>{
+    //todo API call
+    let note = {
+      _id: "683fe7fc1484545b5b21ba58",
+      user: "683ae0203f23d6a97c2b7dfh",
+      title: "Love in Test",
+      description: "A note about the book.[added]",
+      tag: "Book",
+      date: "2025-06-04T06:30:20.854Z",
+    }
+    setNotes([...notes,note])
+  }
+
+  // ----------DELETE a note------------------------>
+
+const deleteNote = () =>{
+
+}
+
+  // -----------EDIT a note------------------------>
+
+const editNote = ()=>{
+
+}
+
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, setNotes, editNote, deleteNote }}>
       {children}
     </NoteContext.Provider>
   );
