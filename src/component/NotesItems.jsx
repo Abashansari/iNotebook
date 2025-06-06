037c5879da78bf1037019f65de2b2a5b66614308
Abash Ansari <ansariabash2004@gmail.com>
2025-06-06 07:57:31 -0400

    used contextAPI

--- src/component/NotesItems.jsx
+++ src/component/NotesItems.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import NoteContext from '../context/notes/NoteContext';
 
 export default function NotesItems({ note }) {
+
+  const {deleteNote,editNote} = useContext(NoteContext)
+
   return (
     <div className="card" style={{width: "18rem"}}>
       <div className="card-body">
@@ -11,8 +15,8 @@ export default function NotesItems({ note }) {
         <a href="#" className="btn btn-primary">{note.tag}</a>
         <div className="mt-2">
 {/* -----------------------------Adding icons--------------------------------------------------------------------------------- */}
-          <FontAwesomeIcon icon={faTrash} style={{ cursor: 'pointer', marginRight: '10px' }} />
-          <FontAwesomeIcon icon={faPenToSquare} style={{ cursor: 'pointer' }} />
+          <FontAwesomeIcon icon={faTrash} style={{ cursor: 'pointer', marginRight: '10px' }} onClick={()=>deleteNote(note._id)}/>
+          <FontAwesomeIcon icon={faPenToSquare} style={{ cursor: 'pointer' }} onClick={()=> editNote(note._id,note.title,note.description,note.tag)}/>
         </div>
       </div>
     </div>
