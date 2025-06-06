de99b9aa5f4f2a367d944b3b33eaafab6c1dc6f2
Abash Ansari <ansariabash2004@gmail.com>
2025-06-06 07:55:16 -0400

    imported cors

--- src/backend/App.js
+++ src/backend/App.js
@@ -2,6 +2,7 @@ import connectToMongo from "./db.js";
 import express from "express";
 import authRoutes from "./routes/auth.js";
 import notesRoutes from "./routes/notes.js";
+import cors from 'cors'
 
 
 connectToMongo()
@@ -9,6 +10,7 @@ connectToMongo()
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
