5903a5b1c398cabc156a80409d5aef98ac1800f0
Abash Ansari <ansariabash2004@gmail.com>
2025-06-12 12:23:27 -0400

    added a handleSubmit function for login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,73 +1,90 @@
-// import * as React from 'react';
-// import { AppProvider } from '@toolpad/core/AppProvider';
+import React from 'react';
+import { AppProvider } from '@toolpad/core/AppProvider';
 
-// import {
-//   SignInPage,
-//    AuthProvider,
-//    AuthResponse,
-// } from '@toolpad/core/SignInPage';
-// import { createTheme } from '@mui/material/styles';
-// import { useColorSchemeShim } from 'docs/src/modules/components/ThemeContext';
-// import { getDesignTokens, inputsCustomizations } from './customTheme';
+import {
+  SignInPage,
+  AuthProvider,
+} from '@toolpad/core/SignInPage';
 
-// // Available sign-in providers
-// const providers = [
-//   { id: 'github', name: 'GitHub' },
-//   { id: 'google', name: 'Google' },
-//   { id: 'credentials', name: 'Email and Password' },
-// ];
+import { createTheme } from '@mui/material/styles';
+import { useColorSchemeShim } from 'docs/src/modules/components/ThemeContext';
+import { getDesignTokens, inputsCustomizations } from './customTheme';
 
-// // Mock sign-in function
-// const signIn = async (provider: AuthProvider): Promise<AuthResponse> => {
-//   return new Promise<AuthResponse>((resolve) => {
-//     setTimeout(() => {
-//       console.log(`Signed in with ${provider.id}`);
-//       resolve({
-//         user: {
-//           id: '123',
-//           name: provider.name,
-//         },
-//       });
-//     }, 500);
-//   });
-// };
+// Available sign-in providers
+const providers = [
+  { id: 'github', name: 'GitHub' },
+  { id: 'google', name: 'Google' },
+  { id: 'credentials', name: 'Email and Password' },
+];
 
-// export default function ThemeSignInPage() {
-//   const { mode, systemMode } = useColorSchemeShim();
-//   const calculatedMode = mode === 'system' ? systemMode || 'light' : mode || 'light';
+// Mock sign-in function
+const signIn = async (provider) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Signed in with ${provider.id}`);
+      resolve({
+        user: {
+          id: '123',
+          name: provider.name,
+        },
+      });
+    }, 500);
+  });
+};
 
-//   const theme = createTheme({
-//     ...getDesignTokens(calculatedMode),
-//     palette: {
-//       mode: calculatedMode,
-//     },
-//     components: {
-//       ...inputsCustomizations,
-//     },
-//   });
+export default function ThemeSignInPage() {
+  const { mode, systemMode } = useColorSchemeShim();
+  const calculatedMode = mode === 'system' ? systemMode || 'light' : mode || 'light';
 
-//   return (
-//     <AppProvider theme={theme}>
-//       <SignInPage
-//         signIn={signIn}
-//         providers={providers}
-//         slotProps={{
-//           form: { noValidate: true },
-//           submitButton: {
-//             color: 'primary',
-//             variant: 'contained',
-//           },
-//         }}
-//         sx={{
-//           '& form > .MuiStack-root': {
-//             mt: '2rem',
-//             rowGap: '0.5rem',
-//             maxWidth: 400,
-//             mx: 'auto',
-//             px: 2,
-//           },
-//         }}
-//       />
-//     </AppProvider>
-//   );
-// }
+  const theme = createTheme({
+    ...getDesignTokens(calculatedMode),
+    palette: {
+      mode: calculatedMode,
+    },
+    components: {
+      ...inputsCustomizations,
+    },
+  });
+
+  const handleSubmit = async(e) =>{
+    try {
+        e.preventDefault()
+    const response = await fetch(`http://localhost:5000/api/auth/login`,{
+        method : 'GET',
+        headers:{
+            'Content-Type':"application/json"
+        }
+    })
+    const json = await response.json()
+    console.log(json)
+    } catch (error) {
+        res.send(404).json({error:error.message})
+    }
+    
+  }
+  return (
+    <AppProvider theme={theme}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{
+          form: { noValidate: true },
+          submitButton: {
+            color: 'primary',
+            variant: 'contained',
+            onClicked:{handleSubmit}
+          },
+        }}
+        sx={{
+          '& form > .MuiStack-root': {
+            mt: '2rem',
+            rowGap: '0.5rem',
+            maxWidth: 400,
+            mx: 'auto',
+            px: 2,
+          },
+        }}
+      />
+    </AppProvider>
+  );
+}
