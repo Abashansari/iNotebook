7327b7628c24e2c6eed2e773abcbf058682f5d1f
Abash Ansari <ansariabash2004@gmail.com>
2025-06-22 11:52:35 -0400

    Added Login Component to iNotebook/mjnj

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import {useNavigate } from 'react-router-dom'
 
 export default function Login() {
 
   const [credentials, setCredentials] = useState({ email:"", password:""})
 
+  let navigate = useNavigate()
+
   const handleSubmit = async (e) => {
-    
+
     e.preventDefault()
     try {
       const response = await fetch(`http://localhost:5000/api/auth/login`, {
@@ -15,8 +18,18 @@ export default function Login() {
         },
         body: JSON.stringify({ email: credentials.email, password: credentials.password })
       })
+
       const json = await response.json()
       console.log(json)
+
+      if(json){
+
+        localStorage.setItem("token",json.authtoken)
+        navigate('/')
+      }else{
+        alert("Invalid Credentials")
+      }
+
     } catch (error) {
       console.log({ error: error.message })
     }
@@ -30,13 +43,13 @@ export default function Login() {
     <div>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="Form-label">Email address</label>
-          <input type="email" className="Form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
           <div id="emailHelp" className="htmlForm-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="Form-label">Password</label>
-          <input type="password" className="Form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
         </div>
         <button type="submit" className="btn btn-primary" >Submit</button>
       </form>
