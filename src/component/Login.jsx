dd7a21d4961109f7a3ab8dc3baff2f1ed81d0104
Abash Ansari <ansariabash2004@gmail.com>
2025-06-22 11:28:06 -0400

    Fix: Correct fetch URL and initialize form state

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,14 +1,19 @@
-import { Try } from '@mui/icons-material'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
-  const handleSubmit = async () => {
+
+  const [credentials, setCredentials] = useState({ email:"", password:""})
+
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault()
     try {
-      const response = await fetch(`localhost:5000/api/auth/login`,{
-        Method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        }
+      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: credentials.email, password: credentials.password })
       })
       const json = await response.json()
       console.log(json)
@@ -16,21 +21,25 @@ export default function Login() {
       console.log({ error: error.message })
     }
   }
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="Form-label">Email address</label>
-        <input type="email" className="Form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" className="htmlForm-text">We'll never share your email with anyone else.</div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="Form-label">Password</label>
-        <input type="password" className="Form-control" id="exampleInputPassword1" />
-      </div>
-
-      <button type="submit" className="btn btn-primary" >Submit</button>
-    </form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="Form-label">Email address</label>
+          <input type="email" className="Form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
+          <div id="emailHelp" className="htmlForm-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="Form-label">Password</label>
+          <input type="password" className="Form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
+        </div>
+        <button type="submit" className="btn btn-primary" >Submit</button>
+      </form>
     </div>
   )
 }
