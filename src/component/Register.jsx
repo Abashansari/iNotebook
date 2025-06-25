35d88e3d21f538994e3d5b35e707acb05666ad2f
Abash Ansari <ansariabash2004@gmail.com>
2025-06-25 08:21:20 -0400

    added a signup/register form

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <form>
+  <div className="mb-3">
+    <label forhtml="Email" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label forhtml="Password" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3">
+    <label forhtml="ConfirmPassword" className="form-label">Confirm Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+    </div>
   )
 }
